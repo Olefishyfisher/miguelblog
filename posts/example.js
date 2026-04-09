838cefe0dbb60d544bbda4a71b05ec0791c289e2
Olefishyfisher <59668763+Olefishyfisher@users.noreply.github.com>
2026-04-09 02:21:35 -0400

    Add files via upload

---
+++ posts/example.js
@@ -0,0 +1,33 @@
+registerPost({
+  "slug": "example",
+  "title": "This is an example. Blah Blah Blah.",
+  "subtitle": "This is an example subtitle. I love Grace.",
+  "doc": "DOC_T3ST",
+  "source": "TEMPLE UNIVERSITY_PHILADELPHIA",
+  "tags": "TESTING1-2-3",
+  "image": null,
+  "imageCaption": "this is an example image caption",
+  "body": [
+    {
+      "type": "text",
+      "content": "give a man a fish"
+    },
+    {
+      "type": "images2col",
+      "items": [
+        {
+          "image": null,
+          "caption": ""
+        },
+        {
+          "image": null,
+          "caption": ""
+        }
+      ]
+    },
+    {
+      "type": "heading",
+      "content": "this is a heading"
+    }
+  ]
+});
