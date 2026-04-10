f0a6fdc4ae105d2551d50e3bd34f8e5ea7170d1e
Olefishyfisher <59668763+Olefishyfisher@users.noreply.github.com>
2026-04-09 21:41:41 -0400

    Update manifest.js

--- posts/manifest.js
+++ posts/manifest.js
@@ -2,6 +2,7 @@
 // Add the slug (filename without .js) of each post here, in order.
 // The site loads them top-to-bottom, so newest first = newest at top of index.
 window.POST_MANIFEST = [
+  'this-is-the-third-test-for-the-site',
   'fear-and-loathing-western-mass',
   'example',
   'what-am-i-doing',
